d5ea86f0ba7baa5af0f02f9f7e6b690c8177d45b
anh071299 <74811191+anh071299@users.noreply.github.com>
2021-07-21 10:17:12 +0700

    ok

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -103,3 +103,31 @@ $('.partner-slick').slick({
         }]
 });
 
+$('.history-about').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        appendDots: $('.history-dots'),
+    }
+)
+$('.history-wrap .slick-dots li:nth-child(1)').html('<a>2005</a>');
+$('.history-wrap .slick-dots li:nth-child(2)').html('<a>2008</a>');
+$('.history-wrap .slick-dots li:nth-child(3)').html('<a>2015</a>');
+$('.history-wrap .slick-dots li:nth-child(4)').html('<a>2020</a>');
+$('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');  
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 2500,
+      easing:'swing',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+    });  
+  });
