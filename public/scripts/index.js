7153f5ffdee49b91deaddd180c49853824e4975f
anh071299 <74811191+anh071299@users.noreply.github.com>
2021-08-07 20:56:50 +0700

    'ok'

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -54,22 +54,21 @@ let xhr = $.ajax({
         slidesToScroll: 4,
         responsive: [
           {
-            breakpoint: 1024,
+            breakpoint: 1200,
             settings: {
               slidesToShow: 3,
               slidesToScroll: 3,
-              dots: true
             }
           },
           {
-            breakpoint: 600,
+            breakpoint: 992,
             settings: {
               slidesToShow: 2,
               slidesToScroll: 2
             }
           },
           {
-            breakpoint: 480,
+            breakpoint: 768,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1
