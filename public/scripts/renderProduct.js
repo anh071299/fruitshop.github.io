7e086813e4c895a2690f6a58cef2b51628dba52c
anh071299 <74811191+anh071299@users.noreply.github.com>
2021-10-07 20:58:50 +0700

    ok

--- public/scripts/renderProduct.js
+++ public/scripts/renderProduct.js
@@ -1,17 +1,1 @@
-export default (p) => 
-` <div class="product-item"  >
-<div class="product-container">
-  <div class="product-img">
-  <a href="product.html?productId=${p.id}">
-    <img src="${p.image}" alt="quả bơ" />
-    </a>
-  </div>
-        <div class="product-content">
-        <h2 class="product-title"><a href="product.html?productId=${p.id}">${p.title}</a></h2>
-   ${p.salePrice == "" ? `<span class ="original-price real-price">$${p.originalPrice}.0</span>` : `<span class ="original-price"><strike>$${p.originalPrice}.0</strike></span>`}
-  <span ${p.salePrice == "" ? `style ="display:none"` : `style = "display:inline" class="sale-price real-price"`}  >$${p.salePrice}.0</span>
-        </div>
-        <a class="basket-icon icon" onclick="addCart(${p.id})"><i class="bi bi-basket"></i></span></a>
-</div>
-</div>`
-    
+export default i=>` <div class="product-item"  >\n<div class="product-container">\n  <div class="product-img">\n  <a href="product.html?productId=${i.id}">\n    <img src="${i.image}" alt="quả bơ" />\n    </a>\n  </div>\n        <div class="product-content">\n        <h2 class="product-title"><a href="product.html?productId=${i.id}">${i.title}</a></h2>\n   ${""==i.salePrice?`<span class ="original-price real-price">$${i.originalPrice}.0</span>`:`<span class ="original-price"><strike>$${i.originalPrice}.0</strike></span>`}\n  <span ${""==i.salePrice?'style ="display:none"':'style = "display:inline" class="sale-price real-price"'}  >$${i.salePrice}.0</span>\n        </div>\n        <a class="basket-icon icon" onclick="addCart(${i.id})"><i class="bi bi-basket"></i></span></a>\n</div>\n</div>`;
