a134aee642533652aba4578b8d8ae971e9f4d740
anh071299 <74811191+anh071299@users.noreply.github.com>
2021-10-21 21:49:35 +0700

    'ok'

--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -1,17 +1,1 @@
-var items = $(".blog-list .blog-post");
-    var numItems = items.length;
-    var perPage = 3;
-
-    items.slice(perPage).hide();
-
-    $('.pagination-container').pagination({
-        items: numItems,
-        itemsOnPage: perPage,
-        prevText: "&laquo;",
-        nextText: "&raquo;",
-        onPageClick: function (pageNumber) {
-            var showFrom = perPage * (pageNumber - 1);
-            var showTo = showFrom + perPage;
-            items.hide().slice(showFrom, showTo).show();
-        }
-});
+export default i=>` <div class="product-item"  >\n<div class="product-container">\n  <div class="product-img">\n  <a href="product.html?productId=${i.id}">\n    <img src="${i.image}" alt=${i.title} />\n    </a>\n  </div>\n        <div class="product-content">\n        <h2 class="product-title"><a href="product.html?productId=${i.id}">${i.title}</a></h2>\n   ${""==i.salePrice?`<span class ="original-price real-price">$${i.originalPrice}.0</span>`:`<span class ="original-price"><strike>$${i.originalPrice}.0</strike></span>`}\n  <span ${""==i.salePrice?'style ="display:none"':'style = "display:inline" class="sale-price real-price"'}  >$${i.salePrice}.0</span>\n        </div>\n        <a class="basket-icon icon" title = "${i.title}" onclick="addCart(${i.id})"><i class="bi bi-basket"></i></span></a>\n</div>\n</div>`;
